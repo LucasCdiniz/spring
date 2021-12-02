f56e42d6523ed91baad901808b54f850f1fd9571
Lucas Diniz <lucascelestino.diniz@gmail.com>
2021-12-02 08:13:51 -0300

    Projeto Blog Pessoal 1

---
+++ blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,73 @@
+package br.org.generation.blogpessoal.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity //create table
+@Table(name = "tb_postagens")// nome da tabela
+public class Postagem {
+
+	//Long - com L maiúsculo classe que vai criar objetos
+	
+	@Id //Informando que este atributo é uma PK
+	@GeneratedValue(strategy  = GenerationType.IDENTITY) //Referente ao auto_increment
+	private Long id;
+	
+	
+	@NotBlank(message = "O atributo Título é obrigatório!")// Não pode ser nulo e branco - forçando a digitar o título
+	@Size(min = 5, max = 100, message = "O atributo título deve ter no mínimo 5 e no máximo 100 caracteres")
+	private String titulo;
+	
+	@NotNull(message = "O atributo texto é obrigatório!")
+	@Size(min = 10, max = 1000, message = "O atributo texto deve ter no mínimo 10 e no máximo 1000 caracteres")
+	private String texto;
+	
+	@UpdateTimestamp
+	private LocalDate data;
+
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+	
+	
+	
+}
